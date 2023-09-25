b24f82148f8ff7078f21f6fa09f330e54c221be2
Moraj <109418239+morajdhwaj@users.noreply.github.com>
2023-09-25 18:33:11 +0530

    [ADD] Contact info and Together components and make it responsive .

--- src/pages/partner-page/Partner.js
+++ src/pages/partner-page/Partner.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './_partner.scss';
 import Related from '../../components/related/Related';
+import Together from '../../components/together/Together';
+import ContactInfo from '../../components/contact-info/ContactInfo';
 
 const Partner = () => {
   return (
     <div className='partner'>
       <Related />
+      <Together />
+      <ContactInfo />
     </div>
   );
 };
